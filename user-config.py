c5c912ae80d9a1c8c156616de6d11848d06a5a4c
CoolDragon675 <s201501052@gmail.com>
2026-03-28 00:27:07 +0800

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -2,6 +2,6 @@ family_files = {}
 
 family = 'miraheze'
 mylang = 'industrialist'
-family_files['miraheze'] = 'https://industrialist.miraheze.org'
+family_files['miraheze'] = 'https://industrialist.miraheze.org/w/api.php'
 usernames['miraheze']['industrialist'] = 'TRCDBot'
 password_file = 'passwordfile'
